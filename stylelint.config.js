8aa37e72c4020edb306786e0c68aa649c3ef00c0
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2026-03-16 20:20:01 +0000

    chore: update stylelint rules and comment out image rendering in category template

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,11 +7,18 @@ module.exports = {
     reportNeedlessDisables: true,
 
     rules: {
+        'color-function-alias-notation': null,
+        'color-function-notation': null,
         'declaration-no-important': true,
         'function-url-no-scheme-relative': true,
         'no-descending-specificity': null,
         'no-unknown-animations': true,
         'no-unknown-custom-media': true,
         'no-unknown-custom-properties': true,
+        'media-feature-range-notation': null,
+        'selector-class-pattern': null,
+        'selector-id-pattern': null,
+        'scss/dollar-variable-pattern': null,
+        'keyframes-name-pattern': null,
     },
 };
